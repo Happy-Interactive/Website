993c93e3d6ab3c4be37e7ef5861c5995562b14ce
Dylan Guidry <dylanguidry@Dylans-Laptop.attlocal.net>
2024-06-18 18:57:30 -0500

    home and threejs env created

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
-const MyComponent = () => {
+const Navbar = () => {
   return (
-    <div>
-      <h1>MyComponent</h1>
-      <p>This is a new component.</p>
-    </div>
+    <nav className="bg-red-600 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-lg font-bold">Happy Interactive.</div>
+        <ul className="flex space-x-4">
+          <li><a href="/" className="text-white hover:text-red-300">Home</a></li>
+          <li><a href="/about" className="text-white hover:text-red-300">About</a></li>
+          <li><a href="/services" className="text-white hover:text-red-300">Services</a></li>
+          <li><a href="/contact" className="text-white hover:text-red-300">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
-export default MyComponent;
+export default Navbar;
