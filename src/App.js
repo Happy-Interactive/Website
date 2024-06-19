27acc64daf1b9bef6efc48436ff2058a49169f3c
Cody Cogbill <116766995+GLVMR64@users.noreply.github.com>
2024-06-18 19:21:55 -0500

    fix error for import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './Navbar';
 import HomePage from './pages/HomePage';
 
 const App = () => {
